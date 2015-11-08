1c73c5ec552868f4f84091c3d5388540a6a79052
Michael McMillan <email@michaelmcmillan.net>
2015-11-08 18:23:34 +0100

    show dialog when backup is due, also included cron-line

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Put the following line in cron.
+# 0 20 * * * bash /Users/michaelmcmillan/scripts/backup.sh >> /Users/michaelmcmillan/scripts/backup.log 2>&1
+
+# Google Drive client
 DRIVE=/usr/local/bin/drive
 
 # What to backup on local disk. 
@@ -12,5 +16,15 @@ day=$(date +%Y-%m-%d:%H:%M)
 hostname=$(hostname -s)
 archive_file="$hostname-$day.tgz"
 
+# Print the date
+date
+
+# Popup-notifier
+osascript -e 'tell app "System Events" to display dialog "Backup starting. Keep in mind."' 2>&1
+
 # Backup the files using tar and piping it to Google Drive.
 tar czfP - $backup_files | $DRIVE upload --stdin --title $archive_file --parent $dest 
+
+# Print success
+echo "Backup complete!"
+echo 
