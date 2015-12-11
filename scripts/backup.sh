8a8d13f69356b541afab853d860af3079a047fdb
Michael McMillan <email@michaelmcmillan.net>
2015-12-11 20:33:20 +0100

    make dialog decide if backup should start or not

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -11,6 +11,27 @@ backup_files="/Users/michaelmcmillan/Prosjekter"
 # Where to backup to in Google Drive.
 dest="0B8cFoexRLCHJS3RRM1MtaDRmeDQ"
 
+function confirm_dialog() {
+    local response=$(osascript << EOT
+    tell application "Finder"
+        activate
+        try
+            set response to button returned of (display dialog "$1")
+            if (response is equal to "OK") then
+                set response to 1
+            end if
+        on error errTet number errNum
+            if (errNum is equal to -128) then
+                -- user cancelled - not an error
+                set response to 0
+            end if
+        end try
+        return response
+    end tell
+    EOT)
+    echo $response
+}
+
 # Create archive filename.
 day=$(date +%Y-%m-%d:%H:%M)
 hostname=$(hostname -s)
@@ -20,11 +41,15 @@ archive_file="$hostname-$day.tgz"
 date
 
 # Popup-notifier
-osascript -e 'tell app "System Events" to display dialog "Backup starting. Keep in mind."' 2>&1
+should_backup=$(confirm_dialog "Backup is due. Kick it off?")
 
-# Backup the files using tar and piping it to Google Drive.
-tar czfP - $backup_files | $DRIVE upload --stdin --title $archive_file --parent $dest 
+if [ $should_backup = 1 ]; then
+    
+    # Backup the files using tar and piping it to Google Drive.
+    echo "Starting backup!"
+    tar czfP - $backup_files | $DRIVE upload --stdin --title $archive_file --parent $dest 
 
-# Print success
-echo "Backup complete!"
-echo 
+    # Print success
+    echo "Backup complete!"
+    echo
+fi
